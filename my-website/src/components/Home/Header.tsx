ae497a1a943f2e504c299a7a32556a1a4baf6ce8
Lachlan Williams <lwil0039@student.monash.edu>
2023-07-13 16:01:00 +1000

    react router install

--- my-website/src/components/Home/Header.tsx
+++ my-website/src/components/Home/Header.tsx
@@ -9,7 +9,7 @@ const Header = () => {
             <div className="Header" style={{width: 1512, height: 135, position: 'relative'}}>
                 <div className="HeaderBg" style={{width: 1512, height: 135, left: 0, top: 0, position: 'absolute', background: '#252525'}} />
                 <div className="HomeIcon" style={{width: 60, height: 60, left: 45, top: 37, position: 'absolute'}}>
-                <img src={HomeIcon} alt="Home Icon" style={{ width: '100%', height: '100%' }} />
+                    <img src={HomeIcon} alt="Home Icon" style={{ width: '100%', height: '100%' }} />
                 </div>
                 <div className="HeaderText" style={{width: 397, height: 32, left: 1001, top: 51, position: 'absolute'}}>
                     <div className="About" style={{width: 93, height: 32, left: 304, top: 0, position: 'absolute', color: 'white', fontSize: 30, fontFamily: 'Inter', fontWeight: '500', wordWrap: 'break-word'}}>About</div>
