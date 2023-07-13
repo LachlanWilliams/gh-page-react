52bc9fdbf28ddf69c72a9a433029caf3a7dab252
Lachlan Williams <lwil0039@student.monash.edu>
2023-07-13 12:42:03 +1000

    Header and home components

--- my-website/src/components/Home/Header.tsx
+++ my-website/src/components/Home/Header.tsx
@@ -1,9 +1,11 @@
 import react from 'react';
-import { Box } from '@mui/material';
+import { Box, Typography } from '@mui/material';
 
 const Header = () => {
     return (
        <Box>
+            <Typography variant='h3'>Lachlan Williams</Typography>
+            <Typography variant='h3'>Home</Typography>
          
        </Box>
     );
---
+++ my-website/src/components/Home/Home.tsx
@@ -0,0 +1,12 @@
+import { Box, Typography } from '@mui/material';
+import Header from './Header';
+
+const Home = () => {
+    return (
+       <Box>
+         <Header />
+       </Box>
+    );
+    }
+
+export default Home
