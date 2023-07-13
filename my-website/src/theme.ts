7555474dfe7074ed7d1fbfe818b015709aa997eb
Lachlan Williams <lwil0039@student.monash.edu>
2023-07-13 13:41:10 +1000

    Theme.ts file

---
+++ my-website/src/theme.ts
@@ -0,0 +1,10 @@
+import { createTheme } from "@mui/material/styles";
+import { light } from "@mui/material/styles/createPalette";
+
+const theme = createTheme({
+    palette: {
+
+    }
+})
+
+export default theme;
