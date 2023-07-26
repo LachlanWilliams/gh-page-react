71f23a4f348dcb0b80701453b6774b41b61373cd
Lachlan Williams <lwil0039@student.monash.edu>
2023-07-26 19:33:42 +1000

    NavBarS fixed

--- my-website/src/components/Home/NavBarS.tsx
+++ my-website/src/components/Home/NavBarS.tsx
@@ -2,15 +2,30 @@ import React from 'react';
 import { Box, Typography, Button } from '@mui/material';
 import MenuRoundedIcon from '@mui/icons-material/MenuRounded';
 import HomeIcon from '../../assets/images/HomeIcon.png';
+import MenuIcon from '../../assets/images/MenuIcon.png';
 
 const NavBarS = () => {
     return (
-       <Box >
+        <Box >
         <nav className="navbar">
             <a href="#" className="logo">
                 <img className="homeIcon" src={HomeIcon} alt="Home Icon" />
             </a>
-            <Button startIcon={<MenuRoundedIcon className='menuIcon' />}/>
+            <input type="checkbox" id="toggler" />
+            <label htmlFor="toggler"><i className="ri-menu-line"></i></label>
+            <a href="#" className="menu">
+                <img className="menuIcon" src={MenuIcon} alt="MenuIcon" />
+            </a>
+        </nav>
+       </Box>
+    );
+    return (
+       <Box >
+        <nav className="navbar">
+            <a href="#" className="menuIcon">
+                <img className="DropMenuIcon" src={MenuIcon} alt="MenuIcon" />
+            </a>
+            {/* <Button startIcon={<MenuRoundedIcon className='menuIcon' />}/> */}
         </nav>
        </Box>
     );
